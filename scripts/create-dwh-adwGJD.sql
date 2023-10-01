bb67d4842d916b91f988dbbd5c93ab91df072fab
David Guzman <drguzman511@gmail.com>
2023-10-01 18:44:11 +0000

    carga

---
+++ scripts/create-dwh-adwGJD.sql
@@ -0,0 +1,85 @@
+# Creación del esquema de la bodega de datos
+
+create database if not exists adw_dwh;
+use adw_dwh;
+
+-- Dimension de tiempo
+create table if not exists dim_time(
+    date_key         int(8)     not null,
+    date_value       date       not null,
+    month_number     int(3)     not null,
+    year4            int(5)     not null,
+    day_of_week      int(5)     not null,
+    day_of_week_name char(12)   not null,
+ 
+    primary key(date_key),
+    unique index date_value (date_value)
+);
+
+-- Dimension Vendedor
+create table if not exists dim_vendedor(
+    salesperson_key       int not null AUTO_INCREMENT,
+    salesperson_id        tinyint(8)  not null, 
+    salesperson_name      varchar(30) not null,
+    city                  varchar(30) not null,
+    country               varchar(30) not null,
+    gender                varchar(5)  not null, 
+    paymentfreq           tinyint(8)  not null,    
+    department            varchar(30)  not null,
+    last_update           timestamp not null default CURRENT_TIMESTAMP,
+
+    primary key (salesperson_key),
+    unique index (salesperson_id)
+
+);
+
+-- Dimension Tienda
+create table if not exists dim_tienda(
+    store_key   int not null AUTO_INCREMENT,
+    store_id    tinyint(8) not null, 
+    store       varchar(20) not null, 
+    last_update timestamp not null default CURRENT_TIMESTAMP,
+
+    primary key (store_key),
+    unique index (store_id)
+);
+
+-- Dimension Producto
+create table if not exists dim_producto(
+    product_key   int not null AUTO_INCREMENT,
+    product_id    tinyint(8) not null, 
+    product       varchar(100) not null,
+    localizacion  varchar(100) not null,
+    costrate      decimal(10,4) not null,
+    class         char(2) not null, 
+    category      varchar(100) not null,
+    subcategory   varchar(100) not null,
+    last_update timestamp not null default CURRENT_TIMESTAMP,
+
+    primary key (product_key),
+    unique index (product_id)
+);
+
+-- Dimension Proveedor
+create table if not exists dim_proveedor(
+    proveedor_key   int not null AUTO_INCREMENT,
+    proveedor_id    tinyint(8) not null, 
+    proveedor       varchar(100) not null,
+    credit_rating   tinyint(1) not null,
+    ship_method     varchar(100) not null,
+    last_update timestamp not null default CURRENT_TIMESTAMP,
+
+    primary key (proveedor_key),
+    unique index (proveedor_id)
+);
+
+-- Dimension Proveedor
+create table if not exists dim_territorio(
+    territory_key   int not null AUTO_INCREMENT,
+    territory_id    int not null, 
+    name_territory  varchar(100) not null,
+    last_update timestamp not null default CURRENT_TIMESTAMP,
+
+    primary key (territory_key),
+    unique index (territory_id)
+);
